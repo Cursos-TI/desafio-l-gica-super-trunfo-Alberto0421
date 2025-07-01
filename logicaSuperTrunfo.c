f42de5d2080ecdcc19d326933712b55de64e261a
Alberto0421 <Monteiro.alberto@yahoo.com.br>
2025-07-01 23:35:02 +0000

    Segundo Trabalho comparação de Cidades por populacao.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -7,7 +7,13 @@
 
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
-    
+    char cartaA[1];
+        char cartaB[1];
+        char cidadeA[15];
+        char cidadeB[15];
+        int populacaoA;
+        int populacaoB;
+
     // Você pode utilizar o código do primeiro desafio
 
     
@@ -15,6 +21,27 @@ int main() {
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
+printf("Carta A:\n");
+
+              printf("Digite a Primeira Cidade: \n");
+              scanf("%s",&cidadeA);
+
+              printf("Digite a Segunda Cidade: \n");
+              scanf("%s",&cidadeB);
+
+              printf("Digite a população da 1º Cidade: \n");
+              scanf("%d", &populacaoA);
+
+              printf("Digite a população da 2º Cidade: \n");
+              scanf("%d", &populacaoB);
+
+printf("A primeira cidade é: %s \n", cidadeA);
+              printf("A sua População é:   %d  \n", populacaoA);
+             
+              printf("A Segunda cidade é:  %s   \n", cidadeB);   
+              printf("A sua População e:    %d   \n",populacaoB);
+
+
 
     // Exemplo:
     // printf("Digite o código da cidade: ");
@@ -32,6 +59,21 @@ int main() {
     // } else {
     //     printf("Cidade 2 tem maior população.\n");
     // }
+                if (populacaoA > populacaoB)
+                {
+                    printf(" A Cidade: %s   Venceu:\n",cidadeA);
+                }
+
+                else
+                {
+                    printf("A Cidade: %s venceu:\n", cidadeB);
+                }
+                
+                
+                
+
+
+
 
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
